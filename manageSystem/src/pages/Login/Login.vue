7d032658d4dcb04493ef3869742e7361f93808ec
majian10 <wx_9914b0477f11435784fd867eaa979524@git.code.tencent.com>
2018-11-22 14:14:08 +0800

    首个版本提交

---
+++ manageSystem/src/pages/Login/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div className="login-page">
+        <SimpleFooter />
+        <UserLogin />
+      </div>
+</template>
+
+<script>
+import SimpleFooter from './components/SimpleFooter';
+import UserLogin from './components/UserLogin';
+
+export default {
+  name: 'Login',
+  components: {
+    SimpleFooter,
+    UserLogin,
+  },
+};
+</script>
